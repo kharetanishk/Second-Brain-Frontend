4b9ea70b7caf30d7b1d5b470e0711b381da35f97
Tanishk khare <kharetanishk18@gmail.com>
2025-07-14 19:01:29 +0530

    Bootstraped Tailwind Css in the project and checked its functioning

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-8xl bg-amber-500 text-black border-2">
+          Hello world
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
